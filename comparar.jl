7b9247ad74bbf3cd6ca747dbf2458af2bf3de780
erenchun <cerenchun@miuandes.cl>
2018-09-30 19:01:08 -0300

    4

--- comparar.jl
+++ comparar.jl
@@ -46,14 +46,58 @@ for hijo in child_nodes(List_of_OSVs)
 	end
 end
 
+a = Z
+println("satelite listo")
+xdoc_in = parse_file("orbita-euler-mejorado.xml")
 
-t1,Z1 = Z
-t2,Z2 = Z
+Earth_Explorer_File= root(xdoc_in) 
+#println(name(Earth_Explorer_File))
 
-println("t2")
+header_list = Earth_Explorer_File["Earth_Explorer_Header"]
+header = header_list[1]
+#println(header)
 
-difft = maximum(t1[1:9000] - t2[1:9000])
-diffX = Z1[1:3,1:9000] - Z2[1:3,1:9000]
+data_block = Earth_Explorer_File["Data_Block"][1]
+List_of_OSVs = data_block["List_of_OSVs"][1]
+
+
+Nt_texto = attribute(List_of_OSVs, "count")
+Nt = parse(Int32, Nt_texto) 
+#println("Nt = $(Nt)")
+t = zeros(Nt)
+ZZ = zeros(6, Nt)
+
+i = 1
+for hijo in child_nodes(List_of_OSVs)
+	global i
+	
+
+	if is_elementnode(hijo)
+		nombre = name(hijo)
+
+		if nombre == "OSV"
+			
+			utc_texto = content(XMLElement(hijo)["UTC"][1])
+			utc_time = Dates.DateTime(utc_texto[5:end], "yyyy-mm-ddTHH:MM:SS.ssssss")
+			UnixTime = Dates.datetime2unix(DateTime(utc_time))
+			#println("i = $(i) utc_texto = $(utc_texto) now = $(now)")
+			t[i] = UnixTime
+			ZZ[1,i] = parse(Float64, content(XMLElement(hijo)["X"][1]))
+			ZZ[2,i] = parse(Float64, content(XMLElement(hijo)["Y"][1]))
+			ZZ[3,i] = parse(Float64, content(XMLElement(hijo)["Z"][1]))
+			ZZ[4,i] = parse(Float64, content(XMLElement(hijo)["VX"][1]))
+			ZZ[5,i] = parse(Float64, content(XMLElement(hijo)["VY"][1]))
+			ZZ[6,i] = parse(Float64, content(XMLElement(hijo)["VZ"][1]))
+			i += 1
+		end	
+	end
+end
+b = ZZ
+println("orbita listo")
+println("SATELITE VS EULER MEJORADO")
+
+difft = maximum(a[1:9000] - b[1:9000])
+diffX = a[1:3,1:9000] - b[1:3,1:9000]
 
 Norm_diffX = sum(sqrt.(diffX .* diffX), dims=1)
 
